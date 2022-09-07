d82fd2197c0d620d65721fe8b87534060abc877e
omoluabidotcom <yyahaya222@yahoo.com>
2022-09-07 14:25:50 +0100

    web security configuration add

--- src/main/java/com/authenticationapplication/config/WebSecurityConfig.java
+++ src/main/java/com/authenticationapplication/config/WebSecurityConfig.java
@@ -1,2 +1,15 @@
-package com.authenticationapplication.config;public class WebSecurityConfig {
+package com.authenticationapplication.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+public class WebSecurityConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(11);
+    }
 }
