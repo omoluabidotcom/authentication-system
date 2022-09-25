bf7fa4f6a9d5729463e9d452a40f7aeafa0c489a
omoluabidotcom <yyahaya222@yahoo.com>
2022-09-25 02:52:43 +0100

    Authorization server add
    All sub-package needed as also been added, user entity add, user repository add and logic to load spring security user db add

---
+++ OAuth-authorization-server/src/main/java/com/authenticationproject/OAuthserver/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.authenticationproject.OAuthserver.repository;
+
+//import com.authenticationapplication.entity.User;
+import com.authenticationproject.OAuthserver.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository
+        extends JpaRepository<User, Long> {
+    User findByEmail(String email);
+}
